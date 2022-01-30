15390a32c76360b8a3b93d83eca322f8ae5c0fe8
礼检 <ljj337009@antgroup.com>
2022-01-30 19:53:39 +0800

    fix: dumi trans loader

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,4 @@
-import { defineConfig } from 'dumi';
-
-export default defineConfig({
+export default {
   title: 'Eva-Design',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
@@ -8,4 +6,37 @@ export default defineConfig({
   outputPath: 'docs-dist',
   mode: 'site',
   // more config: https://d.umijs.org/config
-});
+  styles: [
+    '.__dumi-default-layout-content > .markdown:first-child:empty, button.__dumi-default-icon[role="codesandbox"] { display: none; }',
+  ],
+  menus: {
+    // 需要自定义侧边菜单的路径，没有配置的路径还是会使用自动生成的配置
+    '/components': [
+      {
+        title: '组件',
+        children: [
+          // 菜单子项（可选）
+          // 对应的 Markdown 文件，路径是相对于 resolve.includes 目录识别的
+          'Box',
+          'Sphere',
+        ],
+      },
+    ],
+    // // 如果该路径有其他语言，需在前面加上语言前缀，需与 locales 配置中的路径一致
+    // '/zh-CN/guide': [
+    //   // 省略，配置同上
+    // ],
+  },
+
+  // navs: [
+  //   {
+  //     title: '我有二级导航',
+  //     path: '链接是可选的',
+  //     // 可通过如下形式嵌套二级导航菜单，目前暂不支持更多层级嵌套：
+  //     children: [
+  //       { title: '第一项', path: '/Box' },
+  //       { title: '第二项', path: '/Sphere' },
+  //     ],
+  //   },
+  // ],
+};
---
+++ src/Sphere/index.tsx
@@ -0,0 +1,53 @@
+import React, { useRef, useState } from 'react';
+import { useFrame } from '@react-three/fiber';
+import type { MeshProps } from '@react-three/fiber';
+
+type BoxProps = {
+  length: number;
+  autoRotate: boolean;
+  initRotation: string[];
+  position: string[];
+  scale: number;
+  rotateSpeed: [number, number, number];
+};
+
+export default (props: BoxProps) => {
+  const {
+    autoRotate = false,
+    length = 3,
+    scale = 1,
+    initRotation = [0, 0, 0],
+    position = [0, 0, 0],
+    rotateSpeed = [0.01, 0.005, 0.005],
+  } = props;
+
+  const [xRotateSpeed, yRotateSpeed, zRotateSpeed] = rotateSpeed;
+
+  const ref = useRef<MeshProps>();
+
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+
+  useFrame((state, delta) => {
+    if (autoRotate && ref.current) {
+      ref.current.rotation.x += xRotateSpeed;
+      ref.current.rotation.y += yRotateSpeed;
+      ref.current.rotation.z += zRotateSpeed;
+    }
+  });
+
+  return (
+    <mesh
+      ref={ref}
+      position={position}
+      rotation={initRotation}
+      scale={[scale, scale, scale]}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <sphereGeometry />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+};
